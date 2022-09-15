caec36c98c8da4da25d2e4e4dd5f88a9ef947db6
Jorge <jorandho@gmail.com>
2022-09-15 15:23:19 -0500

    Puesta a punto del carrito de compras

--- proyecto_web/carro/context_processor.py
+++ proyecto_web/carro/context_processor.py
@@ -1,7 +1,10 @@
 # Variable global
 def importe_total_carro(request):
     total = 0
-    #if request.user.is_authenticated:
-    '''for key, value in request.session["carro"].items():
-        total = total + float(value["precio"])'''
+    if request.user.is_authenticated:
+        for key, value in request.session["carro"].items():
+            total = total + float(value["precio"])
+    else:
+        total = "Debes iniciar sesión"
+    
     return {"importe_total_carro": total}
--- proyecto_web/proyecto_web_app/views.py
+++ proyecto_web/proyecto_web_app/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render, HttpResponse
-
+from carro.carro import Carro
 
 # Create your views here.
 
 def home(request):
+    carro = Carro(request)
     return render(request, "proyecto_web_app/home.html")
 
 
